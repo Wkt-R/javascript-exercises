d6956b60bd14019eb0bfc0eade2b74e8f145e325
Wkt-R <wkondraciuk99@gmail.com>
2024-07-23 18:03:11 +0200

    4, 5, 6 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...deleted) {
+    return array.filter(elements => !deleted.includes(elements));
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(firstArg, secondArg) {
+    if(!Number.isInteger(firstArg) || !Number.isInteger(secondArg)){
+        return "ERROR";
+    } else if(firstArg < 0 || secondArg < 0){
+        return "ERROR";
+    } else{
+    return (Math.abs(firstArg-secondArg) + 1)*(firstArg + secondArg)/2;
+    }
 };
 
 // Do not edit below this line
