425ceefcf7c480509576159d4cfdef38f1a5d565
Wkt-R <wkondraciuk99@gmail.com>
2024-07-23 16:43:05 +0200

    1, 2, 3 excersizes done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(msg, number) {
+    if(number < 0){
+        return "ERROR";
+    } else{
+    let finalMsg = msg.repeat(number);
+    return finalMsg;
+    }
 };
 
 // Do not edit below this line
