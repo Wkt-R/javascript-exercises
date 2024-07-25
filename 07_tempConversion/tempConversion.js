958d891b1ecb603092bbc8c4cbda98bd6ea17d46
Wkt-R <wkondraciuk99@gmail.com>
2024-07-25 11:14:33 +0200

    7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  let conv = (degree - 32) / 1.8;
+  return Math.round(conv * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let conv = (degree * 1.8) + 32;
+  return Math.round(conv * 10) / 10;
 };
 
 // Do not edit below this line
